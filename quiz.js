5dc33e9c9d321599ceed8030a1f603f06a0b1b40
ayomide <mikaheelayomide5@gmail.com>
2024-03-08 11:33:54 +0100

    new commit

--- quiz.js
+++ quiz.js
@@ -1,3 +1,68 @@
+correctScore = 10;
+incorrectScore = 0;
+let theObjects = [];
+
+function toSecond() {
+  firstquestion();
+  /*
+  secondquestion();
+let allInput =  document.querySelectorAll("input");
+allInput.forEach(inputs => {
+  console.log(inputs)
+})
+*/
+// console.log(allInput);
+// window.location.href = "quiz2.html";
+}
+
+function firstquestion() {
+  let mainform = document.querySelector(".formone");
+let allinput =
+document.querySelectorAll(".formone p");
+  
+  
+  
+  
+  
+  console.log(allinput)
+}
+
+
+
+
+
+
+function toThird() {
+  console.log("yes");
+  window.location.href = "quiz3.html";
+}
+function toFourth() {
+  console.log("yes");
+  window.location.href = "quiz4.html";
+}
+
+
+
+
+  
+
+/*
+let theObjects = localStorage.setItem("scores", JSON.stringify(eachsores)) || [];
+*/
+
+
+
+// make an object for the scores
+
+function result() { 
+}
+
+
+
+
+
+
+/*
 let totry = {"one":"fatiu", "two":"toriq", "three": "yakubu", "four":"rokeeb"};
 let randomi = "";
 
@@ -5,4 +70,5 @@ for (const x in totry) {
   randomi =+ totry;
   console.log(totry[x]);
 }
+*/
 
